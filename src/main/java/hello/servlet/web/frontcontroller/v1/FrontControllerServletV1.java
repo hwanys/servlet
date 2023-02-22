e4b6e5af4fbd9bbd51eb80ecbf2b1fe090c406b9
hwanys <ksh7353@naver.com>
2023-02-22 16:09:37 +0900

    Front Controller 구현

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,41 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name="frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+
+        String requestURI = request.getRequestURI();    //요청 URI 가져와서 저장
+
+        ControllerV1 controller = controllerMap.get(requestURI);    // 요청 주소에 해당하는 Controller 저장.
+        if (controller == null) {   // 해당 URI와 일치하는 controller가 없을 시.
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404 status의 response return
+            return;
+        }
+
+        controller.process(request, response);  // 호출 시 ControllerV1 interface의 process메소드 호출
+    }
+}
